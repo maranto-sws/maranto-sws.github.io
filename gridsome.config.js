e8ce61657336603ace5bf2b3b0c31aba99b06640
Ryan Balfanz <rbalfanz@gmail.com>
2019-05-07 16:07:42 -0700

    Add site descriprion

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,6 +7,7 @@
 module.exports = {
   siteName: "Maranto’s Sewer & Water Services LLC",
   siteUrl: 'https://maranto-sws.github.io',
+  siteDescription: "If water goes through it, Maranto's can do it!",
   plugins: [
     {
       use: '@gridsome/plugin-google-analytics',
