bbdeb7e2a9c2f0eab9f5d2b3ee72766130a6d7b2
Ryan Balfanz <rbalfanz@gmail.com>
2019-05-08 17:43:49 -0700

    Add og:url

--- src/main.js
+++ src/main.js
@@ -66,12 +66,12 @@ export default function (Vue, { router, head, isClient }) {
   //   content: '@RyanBalfanz',
   // })
 
-  // router.beforeEach((to, _from, next) => {
-  //   head.meta.push({
-  //     key: 'og:url',
-  //     property: 'og:url',
-  //     content: process.env.GRIDSOME_BASE_PATH + to.path,
-  //   })
-  //   next()
-  // })
+  router.beforeEach((to, _from, next) => {
+    head.meta.push({
+      key: 'og:url',
+      property: 'og:url',
+      content: process.env.GRIDSOME_BASE_PATH + to.path,
+    })
+    next()
+  })
 }
